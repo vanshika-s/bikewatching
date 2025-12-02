4784fac0de742ba8d6aa67059580c0ee85aab429
Vanshika S <63630298+vanshika-s@users.noreply.github.com>
2025-12-01 18:26:45 -0800

    Initial Bikewatching setup with base Mapbox map

---
+++ map.js
@@ -0,0 +1,21 @@
+// Import Mapbox as an ESM module
+import mapboxgl from "https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm";
+
+// Set your Mapbox access token here
+mapboxgl.accessToken = "pk.eyJ1IjoidnNvbWFuaTEyMyIsImEiOiJjbWlueWVoZm8wMjdkM2VxMDg3OGd6OHdhIn0.o9Auxw2oA0UGI5m49o0rQw";
+
+// Check that Mapbox GL JS is loaded
+console.log("Mapbox GL JS Loaded:", mapboxgl);
+
+// Initialize the map
+const map = new mapboxgl.Map({
+  container: "map", // ID of the div where the map will render
+  style: "mapbox://styles/mapbox/streets-v12", // Map style
+  center: [-71.09415, 42.36027], // [longitude, latitude] (Boston-ish)
+  zoom: 12, // Initial zoom level
+  minZoom: 5,
+  maxZoom: 18,
+});
+
+// Optional: add zoom + rotation controls
+map.addControl(new mapboxgl.NavigationControl());
